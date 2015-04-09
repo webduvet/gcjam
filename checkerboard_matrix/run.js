2c29b5346436614dfc9efbe1f57ffb93a078b347
Horse <andrej.bartko@gmail.com>
2015-04-09 17:37:24 +0100

    hey

--- checkerboard_matrix/run.js
+++ checkerboard_matrix/run.js
@@ -12,11 +12,55 @@ var input = fs.readFileSync(fileIn, {encoding: "utf-8"});
 var output = ""
 
 var input = input.split(/[ |\n]/g);
+input.pop();
 var cases = input.shift();
 
 console.time('elapsed');
+//console.log(input);
 
 // solve problem
+
+var N = 0,
+	board = null,
+	mat = null,
+	mati = null;
+
+
+for (var ca = 1; ca <= cases; ca++) {
+	
+	N = (+input.shift()) * 2 ;
+	board = [];
+	map = []
+	X = [];
+	invX = [];
+	
+	for (var y = 0; y < N; y++) {
+		board.push(input.shift().split('').map(function(el){
+			//map.push(!!+el);
+			return !!+el;
+		}));
+
+
+		X.push(board[y]);
+		console.log(X[y]);
+		X[y].forEach(function(el, index, arr){
+			arr[index] = !!(index % 2) && el;
+		});
+	}
+
+	console.log(X);
+
+
+
+	// mask it
+
+
+	//console.log(board);
+
+
+}
+
+
 
 
 
