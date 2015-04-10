e5212c0b7b37bc604d0c1cb6bae17150f7b9ddb9
Horse <andrej.bartko@gmail.com>
2015-04-10 11:53:29 +0100

    simple matrix some poperations

--- squareMatrix/sqmatrix.js
+++ squareMatrix/sqmatrix.js
@@ -17,6 +17,29 @@ SqMatrix.prototype.inv = function(){
 	}
 }
 
+SqMatrix.prototype.foldAnd = function(){
+	var val = 0;
+	for (var y = 0; y < this.N-1 ; y++){
+		for (var x = y+1; x < this.N; x++ ) {
+			val = this.matrix[x + y*this.N] && this.matrix[y + x*this.N];
+			this.matrix[x + y*this.N] = val;
+			this.matrix[y + x*this.N] = val;
+		}
+	}
+}
+
+SqMatrix.prototype.foldXorInv = function(){
+	var val = 0;
+	for (var y = 0; y < this.N-1 ; y++){
+		for (var x = y+1; x < this.N; x++ ) {
+			//val = !((!this.matrix[x + y*this.N] && this.matrix[y + x*this.N]) || (this.matrix[x + y*this.N] && !this.matrix[y + x*this.N]));
+			val = this.matrix[x + y*this.N] === this.matrix[y + x*this.N];
+			this.matrix[x + y*this.N] = val;
+			this.matrix[y + x*this.N] = val;
+		}
+	}
+}
+
 SqMatrix.prototype.increment = function(){
 	for(var z = 0; z < this.L; z++){
 		this.matrix[z] = z+1;
@@ -39,6 +62,30 @@ SqMatrix.prototype.makeCheckerBoard = function(){
 	}
 }
 
+SqMatrix.prototype.and = function(B){
+	if (! this.L === B.L) throw new Error("matrices must be of the same size");
+	var C = new SqMatrix(this.N);
+	for(var z = 0; z < this.L; z++){
+		C.matrix[z] = this.matrix[z] && B.matrix[z];
+	}
+	return C;
+}
+
+/**
+ * match is the same as inv xor
+ */
+SqMatrix.prototype.match = function(B){
+	if (! this.L === B.L) throw new Error("matrices must be of the same size");
+	var C = new SqMatrix(this.N);
+	for(var z = 0; z < this.L; z++){
+		//C.matrix[z] = !((!this.matrix[z] && B.matrix[z]) || (this.matrix[z] && !B.matrix[z]));
+		C.matrix[z] = this.matrix[z] === B.matrix[z];
+	}
+	return C;
+}
+
 var m = new SqMatrix(4);
 
+m.makeCheckerBoard();
+m.foldXorInv();
 console.log(m.matrix);
